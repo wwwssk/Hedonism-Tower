992cb22b6aecf07e52bfc99ad81461fc293f6b5a
KostaSenatov <kostyasenatov@gmail.com>
2022-05-02 17:13:00 +0700

    123

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,8 @@ const { src, dest, parallel, series, watch } = require('gulp');
 
 const browserSync = require('browser-sync').create();
 const concat = require('gulp-concat');
-const uglify = require('gulp-uglify-es').default;
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('gulp-autoprefixer');
-const cleancss = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const newer = require('gulp-newer');
 const del = require('del');
@@ -53,7 +51,6 @@ function scripts() {
 		.pipe(rigger())
 		.pipe(sourcemaps.init())
 		.pipe(concat('main.min.js'))
-		.pipe(uglify())
 		.pipe(sourcemaps.write('./maps'))
 		.pipe(dest('public/js/'))
 		.pipe(browserSync.stream())
@@ -66,7 +63,6 @@ function styles() {
 		.pipe(sass())
 		.pipe(concat('main.min.css'))
 		.pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
-		.pipe(cleancss( { level: { 1: { specialComments: 0 } }/* , format: 'beautify' */ } ))
 		.pipe(sourcemaps.write('./maps'))
 		.pipe(dest('public/css/'))
 		.pipe(browserSync.stream())
